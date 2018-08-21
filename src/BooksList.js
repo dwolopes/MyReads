a765b335900a4f9c49c6995ba0e33d89c5749608
Douglas Lopes <dwillian.lopes@gmail.com>
2018-08-20 22:55:50 -0300

    feat: consuming BooksAPI and Getting all books

--- src/BooksList.js
+++ src/BooksList.js
@@ -1,7 +1,20 @@
 import React, {Component} from 'react'
 import BookShelf from './BookShelf'
+import * as BooksAPI from './BooksAPI'
 
 class BooksList extends Component {
+
+    state = {
+        books: []
+    }
+
+    componentDidMount() {
+        BooksAPI.getAll().then((books) => {
+            this.setState({books})
+            console.log(books)
+        })
+    }
+
     render() {
         return (
             <div className="list-books">
