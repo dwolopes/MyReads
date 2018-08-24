a24b188edf1c7f1680d21eb1f1f2e0b80f9b2318
Douglas Lopes <dwillian.lopes@gmail.com>
2018-08-23 22:47:53 -0300

    feat: adding a link and route to show de search component

--- src/BooksList.js
+++ src/BooksList.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf'
 import * as BooksAPI from './BooksAPI'
 
@@ -55,7 +56,9 @@ class BooksList extends Component {
                     })
                 }
                 <div className="open-search">
-                    <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                    <Link to="/search">
+                        Add a book
+                    </Link>
                 </div>
             </div>
         )
