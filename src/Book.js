72b6ce69cc407ce0af1a5afcdae7f402860218cf
Douglas Lopes <douglas.lopes@odonto.cromg.org.br>
2018-08-29 08:34:37 -0300

    feat: add background image dynamically for each book represented by the app

--- src/Book.js
+++ src/Book.js
@@ -31,7 +31,7 @@ class Book extends Component {
             <li>
                 <div className="book">
                     <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: '{this.props.book.imageLinks.thumbnail}' }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
                     <div className="book-shelf-changer">
                         <select onChange={(event)=>{this.props.onChangeShelf(this.props.book, event.target.value)}}>
                             {
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import BooksGrid from './BooksGrid'
 
 // Applying Stateless Functional Components 
